88bb1ad4ced073cbcd7fc13b92679c739d7cfc32
Jay Patel <kp2401075@gmail.com>
2020-01-06 14:51:54 +1100

    Added Documentation, removed unnessary code, removed hardcoded values and added commandline aguments

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # port_scanner
-port scanner 
+This is a simple port scanner it wrote for understanding how Go routines work
+
+it is fully functioning so feel free to use it
+
+it take either 2 or 3 arginemts
+
+## 2 Arguments
+1: Host
+2: port
+``` 
+./portscanner localhost 80
+``` 
+will check wether port 80 is open on the local machine and will show following if open
+```
+Opened localhost:22
+```
+
+## 3 Arguments
+1: Host
+2: port
+``` 
+./portscanner localhost 10 10000
+``` 
+will check wether ports between 10 and 10000 are open on the local machine and will show following for the open ports
+```
+Opened localhost:22
+Opened localhost:631
+```
--- port_scanner.go
+++ port_scanner.go
@@ -3,54 +3,45 @@ package main
 import (
 	"fmt"
 	"net"
+	"os"
 	"strconv"
 	"time"
 )
 
 func main() {
-	// host -> the remote host
-	// timeoutSecs -> the timeout value
-	host := "pucq-admin-01.networks.in.telstra.com.au"
-	// maximum port number
-	max := 800
-	// minimum port number
-	min := 22
-
-	for i := min; i <= max; i++ {
-
-		go rawConnectSingle(host + ":" + strconv.Itoa(min+i))
-
+	argsWithoutProg := os.Args[1:]
+	// host being assigned from command line agument
+	host := argsWithoutProg[0]
+	if len(argsWithoutProg) == 3 {
+		// maximum port number being passed from the command line arguments
+		max, _ := strconv.Atoi(argsWithoutProg[2])
+		// minimum port number being passed from the command line arguments
+		min, _ := strconv.Atoi(argsWithoutProg[1])
+		for i := min - 1; i <= max; i++ {
+			go rawConnectSingle(host + ":" + strconv.Itoa(i))
+		}
+	} else if len(argsWithoutProg) == 2 {
+		rawConnectSingle(host + ":" + argsWithoutProg[1])
+	} else {
+		fmt.Print("Incorrect number of arguments \n",
+			"This Program requiers atleast 2 aguments 1: hostname & 2: port number \n",
+			"3 Aguments will make it check for the range of ports \n",
+			"1: host, 2: starting port & 3: ending port\n")
 	}
-
-	time.Sleep(time.Second * 2)
-
+	// waiting for go routine to finish
+	time.Sleep(time.Second * 5)
 }
+
+// rawConnectSignal take host name and port combined as a string and see if it it able to conenct to it
 func rawConnectSingle(host string) {
 	//fmt.Println("Function not implemeted")
 	//	fmt.Println("tcp", host, time.Second)
 	conn, err := net.DialTimeout("tcp", host, time.Second)
 	if err != nil {
-		fmt.Println("Connecting error:", err)
+		//	fmt.Println("Connecting error:", err)
 	}
 	if conn != nil {
 		defer conn.Close()
 		fmt.Println("Opened", host)
 	}
 }
-
-// func raw_connect(host string, ports []string) {
-
-// 	for _, port := range ports {
-// 		timeout := time.Second
-// 		//port_string := strconv.Itoa(port)
-// 		//	fmt.Println("tcp", net.JoinHostPort(host, port), timeout)
-// 		conn, err := net.DialTimeout("tcp", net.JoinHostPort(host, port), timeout)
-// 		if err != nil {
-// 			fmt.Println("Connecting error:", err)
-// 		}
-// 		if conn != nil {
-// 			defer conn.Close()
-// 			fmt.Println("Opened", net.JoinHostPort(host, port))
-// 		}
-// 	}
-// }
