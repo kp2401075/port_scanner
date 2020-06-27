b3547fcbfe5d7a82a301641918d19f89b22c3382
Jay Patel <jay.patel.2@team.telstra.com>
2020-06-28 09:22:26 +1000

    fixing if else block

--- port_scanner.go
+++ port_scanner.go
@@ -12,33 +12,36 @@ import (
 func main() {
 	args := os.Args[1:]
 
-	//Creating and adding waitgroup for go routine
-	var wg sync.WaitGroup
-	wg.Add(1)
-
-	// Grabbing hostname/IP from arguments
-	host := args[0]
-
 	if len(args) == 3 {
+		// Grabbing hostname/IP from arguments
+		host := args[0]
 		// Grab min and max from the arguments
 		max, _ := strconv.Atoi(args[2])
 		min, _ := strconv.Atoi(args[1])
-
+		//Creating and adding waitgroup for go routine
+		var wg sync.WaitGroup
+		wg.Add(1)
 		go func() {
 			rawConnectMulti(host, min, max)
-
 			wg.Done()
 		}()
+		// wait before completion of scaning
+		wg.Wait()
 	} else if len(args) == 2 {
+		// Grabbing hostname/IP from arguments
+		host := args[0]
 		rawConnectSingle(host + ":" + args[1])
 	} else {
 		fmt.Print("Incorrect number of arguments \n",
-			"This Program requiers atleast 2 aguments 1: hostname & 2: port number \n",
-			"3 Aguments will make it check for the range of ports \n",
-			"1: host, 2: starting port & 3: ending port\n")
+			"This Program works with wither 2 or 3 aguments \n",
+			"2 Arguments mode :\n",
+			"	1: hostname \n",
+			" 	2: port number \n",
+			"3 Aguments mode: \n",
+			"	1: host \n",
+			"	2: starting port\n ",
+			"	3: ending port\n")
 	}
-	// wait before completion of scaning
-	wg.Wait()
 }
 
 // rawConnectMulti will loop over port range
@@ -51,7 +54,7 @@ func rawConnectMulti(host string, start int, stop int) {
 
 //rawConnectSingle will try to connect a host on given port
 func rawConnectSingle(host string) {
-
+	// Dialing connection to host:port
 	conn, _ := net.DialTimeout("tcp", host, time.Second)
 	// Print if connection can be opened
 	if conn != nil {
